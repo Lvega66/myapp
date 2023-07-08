a7bd83a74bdcfd523eb6da1eb3ffd8272fcf9f87
Lvega66 <ligiavega@hotmail.com>
2023-07-07 20:05:41 -0700

    Estilos de imagen
    //Lvega66

--- Frontend/src/components/Card.jsx
+++ Frontend/src/components/Card.jsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import * as React from 'react';
+import CardMUI from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
 
 function Card({title, description, image}) {
     return (
-      <React.Fragment>
-        <div> Title: {title}</div>
-        <div> Description: {description}</div>
-        <div> 
-            <img src={image} style={{
-                width: 300,
-                height:200
-            }}
-            >
-            </img>
-        </div>
-      </React.Fragment>
+      <CardMUI sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={image}
+          alt="Puentecito"
+        />
+        <CardMedia
+          component="img"
+          height="140"
+          image={image}
+          alt="Puentecito"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </CardMUI>
+  
     );
 }
 export default Card;
